df0c2a9b6ef69f7c7990edc087f76dcc97bc3c30
azganiev <122368396+azganiev@users.noreply.github.com>
2023-01-16 16:01:16 +0300

    Update task01.py

--- task01.py
+++ task01.py
@@ -1,4 +1,6 @@
 # Задача 1 
 # https://www.codewars.com/kata/578c1e2edaa01a9a02000b7f/train/python
 def alias_gen(f_name, l_name):
-    pass
+    if f_name[0].isalpha() and l_name[0].isalpha():
+        return f'{FIRST_NAME[f_name[0].upper()]} {SURNAME[l_name[0].upper()]}'
+    return 'Your name must start with a letter from A - Z.'
